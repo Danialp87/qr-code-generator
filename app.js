1930e25cda63443d84c8477752e26b22353b7050
Danial Parsa <danialparsa87@gmail.com>
2024-12-28 18:02:56 +0200

    Converted to ES modules

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const qr = require("qr-image");
-const fs = require("fs");
+import express from "express";
+import qr from "qr-image";
+import fs from "fs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
